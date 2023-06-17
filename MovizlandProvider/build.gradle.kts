12d1ce2b76a8ee7713d1a1217582a8cb257d800b
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2023-06-17 14:15:50 +0100

    Update build.gradle.kts

--- MovizlandProvider/build.gradle.kts
+++ MovizlandProvider/build.gradle.kts
@@ -10,5 +10,5 @@ cloudstream {
 
     tvTypes = listOf( "TvSeries" , "Movie" , "Anime" , "AsianDrama" )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=movizland.online&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=moviesland.eu&sz=%size%"
 }
